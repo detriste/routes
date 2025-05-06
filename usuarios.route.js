a66a6ff6adf6825a7d27afe586be8fd709f65c33
detriste <detriste2018@gmail.com>
2025-05-06 10:30:59 -0300

    b

--- usuarios.route.js
+++ usuarios.route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const usuarioController = require('../controllers/usuarioController');
+const usuariosController = require('../controllers/usuarioController');
 
 router.post('/cadastro', usuarioController.cadastro);
 
