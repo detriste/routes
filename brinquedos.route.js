73339b6f753c476f74a547b591f641bc7f23f964
detriste <detriste2018@gmail.com>
2025-06-03 17:00:15 -0300

    c

--- brinquedos.route.js
+++ brinquedos.route.js
@@ -3,8 +3,6 @@ const router = express.Router();
 const login = require('../middleware/usuarios.middleware');
 const brinquedoscontroller  = require('../controller/brinquedos.controller');
 
-
-
 router.post ('/', 
     login.required,
     login.userRequired,
